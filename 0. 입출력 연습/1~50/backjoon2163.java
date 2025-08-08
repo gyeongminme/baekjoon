a2154b029b3ddbaf3f1a0247d80192430f32b4c0
조경민 <qwsafdre@naver.com>
2025-08-08 11:31:33 +0900

    1

---
+++ 0. 입출력 연습/1~50/backjoon2163.java
@@ -0,0 +1,12 @@
+package asd;
+import java.util.Scanner;
+public class backjoon2163 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int v1 = scan.nextInt();
+        int v2 = scan.nextInt();
+
+        System.out.println(v1*v2-1);
+
+    }
+}
