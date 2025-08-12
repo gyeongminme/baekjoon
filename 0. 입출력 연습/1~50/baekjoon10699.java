2fe47dfa60adb6d1152804629da1316f7e3ffa2f
Gyeongmin Jo <76791128+gyeongminme@users.noreply.github.com>
2025-08-12 23:32:43 +0900

    Add files via upload

---
+++ 0. 입출력 연습/1~50/baekjoon10699.java
@@ -0,0 +1,10 @@
+package 백준;
+
+import java.time.LocalDate;import java.time.ZoneId;
+
+public class baekjoon10699 {
+    public static void main(String[] args) {
+        LocalDate now = LocalDate.now();
+        System.out.println(now);
+    }
+}
