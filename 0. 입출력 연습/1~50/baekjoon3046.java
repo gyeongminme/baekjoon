2bc9e27bf561d829324f638343360c933b295bcd
Gyeongmin Jo <76791128+gyeongminme@users.noreply.github.com>
2025-08-12 23:20:49 +0900

    Add files via upload

---
+++ 0. 입출력 연습/1~50/baekjoon3046.java
@@ -0,0 +1,15 @@
+package 백준;
+
+import java.util.Scanner;
+
+public class baekjoon3046 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+
+        System.out.println((2*b)-a);
+
+    }
+}
